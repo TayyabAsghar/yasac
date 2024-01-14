b9cd97c736917058f847c944e0dcdc6b4ff6bb01
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-01-14 23:05:21 +0500

    🐛 FIX: Onboarded user flow.

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,18 +1,23 @@
+import { redirect } from 'next/navigation';
 import { currentUser } from '@clerk/nextjs';
 import { UserData } from '@/core/types/user-data';
+import { fetchUser } from '@/lib/actions/user.actions';
 import AccountProfile from '@/components/forms/account-profile';
 
 export default async function Page() {
     const user = await currentUser();
     if (!user) return null;
 
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+
     const userData: UserData = {
         id: user.id,
-        bio: '',
-        name: `${user.firstName ?? ''} ${user.lastName ?? ''}`,
-        image: user.imageUrl,
-        objectId: '',
-        username: user.username ?? '',
+        objectId: userInfo?._id,
+        bio: userInfo ? userInfo?.bio : '',
+        image: userInfo ? userInfo?.image : user.imageUrl,
+        username: userInfo ? userInfo?.username : user.username,
+        name: userInfo ? userInfo?.name : `${user.firstName ?? ''} ${user.lastName ?? ''}`
     };
 
     return (
@@ -23,7 +28,7 @@ export default async function Page() {
             </p>
 
             <section className='mt-9 bg-dark-2 p-10'>
-                <AccountProfile user={userData} btnTitle='Continue' />
+                <AccountProfile user={userData} btnTitle='Submit' />
             </section>
         </main>
     );
