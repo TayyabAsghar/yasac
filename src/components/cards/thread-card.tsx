7c6820be0dcedfd063ad6d744e7bb5c694e865a0
M Tayyab Asghar <muhammadtayyabasghar@gmail.com>
2024-01-14 20:51:18 +0500

    ✨ NEW-FEATURE: Implemented 'Delete Thread' feature.

--- src/components/cards/thread-card.tsx
+++ src/components/cards/thread-card.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
-import { formatDateString } from "@/lib/utils";
-// import DeleteThread from "../forms/DeleteThread";
+import Image from 'next/image';
+import Link from 'next/link';
+import { formatDateString } from '@/lib/utils';
+import DeleteThread from '../forms/delete-thread';
 import { ThreadCard } from '@/core/types/thread-card';
 
 
 export default function ThreadCard(threadData: ThreadCard) {
     return (
         <article
-            className={`flex w-full flex-col rounded-xl ${threadData.isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}`}>
+            className={`flex w-full flex-col rounded-xl ${threadData.isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
             <div className='flex items-start justify-between'>
                 <div className='flex w-full flex-1 flex-row gap-4'>
                     <div className='flex flex-col items-center'>
@@ -28,7 +28,7 @@ export default function ThreadCard(threadData: ThreadCard) {
 
                         <p className='mt-2 text-small-regular text-light-2'>{threadData.content}</p>
 
-                        <div className={`${threadData.isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
+                        <div className={`${threadData.isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
                             <div className='flex gap-3.5'>
                                 <Image src='/assets/heart-gray.svg' alt='heart' width={24} height={24}
                                     className='cursor-pointer object-contain' />
@@ -45,7 +45,7 @@ export default function ThreadCard(threadData: ThreadCard) {
                             {threadData.isComment && threadData.comments.length > 0 && (
                                 <Link href={`/thread/${threadData.id}`}>
                                     <p className='mt-1 text-subtle-medium text-gray-1'>
-                                        {threadData.comments.length} repl{threadData.comments.length > 1 ? "ies" : "y"}
+                                        {threadData.comments.length} repl{threadData.comments.length > 1 ? 'ies' : 'y'}
                                     </p>
                                 </Link>
                             )}
@@ -67,12 +67,12 @@ export default function ThreadCard(threadData: ThreadCard) {
                     {threadData.comments.slice(0, 2).map((comment, index) => (
                         <Image key={index} src={comment.author.image}
                             alt={`user_${index}`} width={24} height={24}
-                            className={`${index !== 0 && "-ml-5"} rounded-full object-cover`} />
+                            className={`${index !== 0 && '-ml-5'} rounded-full object-cover`} />
                     ))}
 
                     <Link href={`/thread/${threadData.id}`}>
                         <p className='mt-1 text-subtle-medium text-gray-1'>
-                            {threadData.comments.length} repl{threadData.comments.length > 1 ? "ies" : "y"}
+                            {threadData.comments.length} repl{threadData.comments.length > 1 ? 'ies' : 'y'}
                         </p>
                     </Link>
                 </div>
---
+++ src/components/forms/delete-thread.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import { deleteThread } from '@/lib/actions/thread.actions';
+
+interface Props {
+    threadId: string;
+    currentUserId: string;
+    authorId: string;
+    parentId: string | null;
+    isComment?: boolean;
+}
+
+export default function DeleteThread({ threadId, currentUserId, authorId, parentId, isComment }: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleDeleteThread = async () => {
+        await deleteThread(JSON.parse(threadId), pathname);
+        if (!parentId || !isComment)
+            router.push('/');
+    };
+
+    if (currentUserId !== authorId || pathname === '/') return null;
+
+    return (
+        <Image src='/assets/delete.svg' alt='delete' width={18} height={18}
+            className='cursor-pointer object-contain'
+            onClick={async () => await handleDeleteThread()} />
+    );
+}
